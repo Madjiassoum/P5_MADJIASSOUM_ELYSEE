5360861b57ccddfa1ad77e3de7a0e75177007394
Madjiassoum <madjiassoum.elysee@hotmail.com>
2021-10-10 08:18:49 +0200

    premier commit apres perte de mon disque dur

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const URL = "http://localhost:3000/api/teddies";
+let URL_API_PRODUITS = "https://oc-p5-api.herokuapp.com/api/teddies";
 document.addEventListener("DOMContentLoaded", () => {
   const produitsListe = document.getElementById("container-produits");
   if (produitsListe) {
-    fetch(URL)
+    fetch(URL_API_PRODUITS)
       .then((res) => res.json())
       .then((response) => {
         response.forEach((produit, index) => {
@@ -11,24 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
         function afficherProduits(produit, index) {
           return `
         <div class="produit"}">            
-                <p class="photo"><img src="${
-                  produit.imageUrl
-                }" alt=""></p>           
-                <h2 class="title">${produit.name}</h2>      
-                <p class="price">${produit.price / 10} €</p>               
-                <p class="description">${
-                  produit.description
-                }</p>                
-                <p class="ahref">
-                    <a href="./pages/produit.html?id=${
-                      produit._id
-                    }" class="button">Voir ce produit</a>                     
-                </p>            
+          <p class="photo"><img src="${produit.imageUrl}" alt=""></p>           
+          <h2 class="title">${produit.name}</h2>      
+          <p class="price">${produit.price / 100} €</p>               
+          <p class="description">${produit.description}</p>                
+          <p class="ahref">
+              <a href="./pages/produit.html?id=${
+                produit._id
+              }" class="button">Voir ce produit</a>                     
+          </p>            
         </div>
         `;
         }
       })
       .catch((erreur) => console.log(erreur));
   }
-  console.log(produitsListe);
+  // console.log(produitsListe);
 });
--- src/produit.js
+++ src/produit.js
@@ -1,108 +1,158 @@
-alert("bienvenue dans Produit");
+let url = "https://oc-p5-api.herokuapp.com/api/teddies";
 function $_GET(param) {
   const url = new URL(window.location.href);
   return url.searchParams.get(param);
 }
 
-const API_URL = `http://localhost:3000/api/teddies/${$_GET("id")}`;
+const API_UN_PRODUIT = `https://oc-p5-api.herokuapp.com/api/teddies/${$_GET(
+  "id"
+)}`;
 
 document.addEventListener("DOMContentLoaded", () => {
   const produitSeul = document.getElementById("produit");
-
   if (produitSeul) {
-    fetch(API_URL)
+    fetch(API_UN_PRODUIT)
       .then((res) => res.json())
-      .then((data) => renderProduit(data));
+      .then((response) => {
+        renderProduit(response);
+
+        //! Bouton ajouter au panier
+        let ajouterPanier = document.querySelector(".btnEnvoyer");
+        ajouterPanier.addEventListener("click", (event) => {
+          event.preventDefault();
+
+          // let _id = urlSearchParams.get("id");
+          const idUrl = window.location.search;
+          console.log(idUrl);
+          //! methode 1
+          // const leId = idUrl.slice(1);
+          // console.log(leId);
+          //!  methode 2
+          let urlSearchParams = new URLSearchParams(idUrl);
+          console.log(urlSearchParams);
+
+          // selection id #quantiteProduit
+          const varQte = document.querySelector("#quantiteProduit");
+          const valVarQte = varQte.value;
+
+          // selection id #choix_couleur
+          const varCouleur = document.querySelector("#choix_couleur");
+          const valVarCouleur = varCouleur.value;
+
+          // Affichage ou calcul Prix total
+          const prixTotal = valVarQte * (response.price / 100);
+
+          //Création d'un objet représentant le produit sélectionné
+          let reponse = {
+            _id: response._id,
+            title: response.name,
+            colors: valVarCouleur,
+            price: response.price / 100,
+            quantity: 1 * valVarQte,
+            prixCetteQuantite: prixTotal,
+          };
+          console.log(reponse);
+          // selection id produit
+          const leId = urlSearchParams.get("id");
+
+          // console.log(prixTotal);
+
+          console.log(
+            leId +
+              " " +
+              response.name +
+              " " +
+              valVarCouleur +
+              " " +
+              valVarQte +
+              " " +
+              response.price / 100 +
+              "€ " +
+              prixTotal
+          );
+
+          //! LOCAL STORAGE
+          /* declaration de la variable monStockage dans laquelle on met les clés*/
+          let monStockage = JSON.parse(localStorage.getItem("produit"));
+          // console.log(monStockage);
+          let addPdtLocalStorage = () => {
+            /*ajout dans le tableau de l'objet avec les values choisi par l'utilisateur*/
+            monStockage.push(reponse);
+            /*Transformation en forme JSON 
+                et envoi dans le local storage   */
+            localStorage.setItem("produit", JSON.stringify(monStockage));
+            console.log(monStockage);
+          };
+
+          // ! fonction popupConfirmation
+          const popupConfirmation = () => {
+            if (
+              window.confirm(
+                response.name +
+                  ", " +
+                  valVarCouleur +
+                  `: bien ajouté au panier.
+                Cliquez sur Ok = voir PANIER ou Annuler = retour Accueil `
+              )
+            ) {
+              window.location.href = "panier.html";
+            } else {
+              window.location.href = "../index.html";
+            }
+          };
+          // ! s'il y a deja produits enreg dans localstorage
+          if (monStockage) {
+            addPdtLocalStorage();
+            popupConfirmation();
+          }
+          // ! s'il n'y a pas de produits enreg dans local storage
+          else {
+            monStockage = [];
+            addPdtLocalStorage();
+            popupConfirmation();
+          }
+        });
+      });
   }
+
   function renderProduit(produit) {
-    produitSeul.innerHTML = `                  
-        <p class="photo"><img src="${produit.imageUrl}" alt=""></p>           
-        <h2 class="title">${produit.name}</h2> 
-        <div class="divCouleur">
-          <label for="choix_couleur">Choisir couleur</label> 
-          <select id="choix_couleur" class="choix_couleur" name="choix_couleur">
+    produitSeul.innerHTML = `    
+      <p class="photo"><img src="${produit.imageUrl}" alt=""></p>           
+      <h2 id="title" class="title">${produit.name}</h2>
+      <div class="lesOptions">
+        <form>
+          <label for="choix_couleur">Choix couleur</label> 
+          <select id="choix_couleur" name="choix_couleur">
             ${produit.colors
               .map((color) => `<option>${color}</option>`)
               .join(", ")}
           </select>
-        </div>
-        <p class="price">${produit.price / 100} €</p>               
-        <p class="description">${produit.description}</p>
+        </form>        
+        <form>
+          <label for="quantiteProduit">Choix quantité:</label>
+          <select name="quantite" id="quantiteProduit">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option> 
+            <option value="11">11</option>
+            <option value="12">12</option>                        
+          </select>
+        </form>
+      </div>
+      <p id="price" class="price">${produit.price / 100} €</p>               
+      <p class="description">${produit.description}</p>              
+      <div class="container divbtn">
+        <button id="btnEnvoyer" class="btnEnvoyer btn ajout" type="submit">
+          Ajout au panier
+        </button>         
+      </div>  
         `;
   }
 });
-
-//!obtention de prix total
-document.addEventListener("DOMContentLoaded", () => {
-  const nbChoisi = document.getElementById("divContainQtePrix");
-
-  if (nbChoisi) {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => renderProduit(data));
-  }
-  function renderProduit(produit) {
-    nbChoisi.innerHTML = ` 
-        <p class="pQuantiteProduit" id="pQuantiteProduit">
-            <label for="quantiteProduit">Quantité :</label>
-            <select name="quantite" id="quantiteProduit" class="quantiteProduit">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </p>                 
-        <p class="price"><strong>Total:</strong><span>${
-          produit.price / 100
-        }</span> €</p>        
-        `;
-  }
-});
-//! Selection couleur
-//* sélection des objectifs---------------------------------------------------
-let choisirColor = document.getElementById("choix_couleur");
-document.addEventListener("change", function (e) {
-  choisirColor = e.target.value;
-});
-//! sélection de la quantité
-let quantiteProduit = document.getElementById("number");
-document.addEventListener("change", function (e) {
-  quantiteProduit = e.target.value;
-  console.log(quantiteProduit);
-});
-
-//! Bouton ajouter au panier
-let btnAjouterAuPanier = document.querySelector(".ajout");
-btnAjouterAuPanier.addEventListener("click", () => {
-  btnAjouterAuPanier.style.border = "2px solid blue";
-  btnAjouterAuPanier.style.color = "blue";
-  alert("Produit bien ajouté au panier");
-});
-console.log(btnAjouterAuPanier);
-
-//? Récup et envoi données
-
-//? selection de l'id du formulaire
-// const idCouleur = document.querySelector(".choix_couleur");
-// const choixCouleur = idCouleur.value;
-// console.log(choixCouleur);
-//! Ajouter des données dans le local Storage
-// localStorage.setItem("titre", "valeur title");
-// localStorage.setItem("prenom", "valeur prenom");
-// localStorage.setItem("lieu", "valeur lieue");
-// localStorage.setItem("couleur", "valeur couleur");
-// localStorage.setItem("nom", "valeur nom");
-
-// //! récupérer une donnée du local storage
-// let maDonnee = localStorage.getItem("prenom");
-// console.log(maDonnee);
-
-// //! Supprimer une donnée du local storage
-// localStorage.removeItem("lieu");
-// localStorage.removeItem("nom");
-// localStorage.removeItem("lieu");
-// localStorage.removeItem("titre");
-// localStorage.clear();
-
-// console.log(localStorage);
